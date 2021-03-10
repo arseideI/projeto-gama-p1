8d9944af18bff5502074b7f4b38387268443bb22
igorobs21 <igor21.barbosa@gmail.com>
2021-03-10 10:58:28 -0300

    Modificações em métodos get/post

--- covid_update.py
+++ covid_update.py
@@ -10,6 +10,8 @@
 import pyodbc
 import requests
 from conexao_bd import ConexaoBD
+import datetime
+import asyncio
 
 # conn = pyodbc.connect('Driver={SQL Server};'
 #                       'Server=arseideltest.database.windows.net;'
@@ -19,13 +21,29 @@ from conexao_bd import ConexaoBD
 
 # cursor = conn.cursor()
 
+# db = ConexaoBD('casadocodigo-sql-srv-isr.database.windows.net', 'BD_COVID_GAMMA', 'Administrador', 'Alura!123', 'sqlserver')
+
 class CovidUpdate(ConexaoBD):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, Server, BD, Login, Senha, verbose, driver='Driver = {SQL Server}'):
+        super().__init__(Server, BD, Login, Senha, verbose, driver=driver)
+        self.Server = Server
+        self.BD = BD
+        self.uid = Login
+        self.pwd = Senha
+        self.verbose = verbose
+        self.driver = driver
+        self.data_collected = []
+        self.getUpdate(self)
     
-    def __getUpdate(self):
-        pass
+    # Método que realiza GET request sobre as últimas atualizações de todos os países e armazena o json
+    async def getUpdate(self):
+        # Armazena em data
+        await self.data_collected.append({"data": datetime.now(), "update_collected": requests.get("").json()})
+    
+    # Método que contém a function organiza os dados coletados e submete ao BD
     def postUpdate(self):
-        self.__getUpdate()
+        conn = pyodbc.connect(self.driver + self.Server + self.BD + self.uid + self.pwd)
+        cursor = conn.cursor()
+        self.getUpdate()
 
 
